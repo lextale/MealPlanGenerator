061e232b2817ba78fbde477f439ae34674c701ac
Αλεξάνδρα Παραμύθα <43947733+lextale@users.noreply.github.com>
2025-09-10 00:48:03 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -465,7 +465,7 @@ def save_profile_settings():
     gender = request.form['gender']
     birthday = request.form['dob']
     
-    id_token = user['idToken']
+    id_token = session['user']['idToken']
     # Αποθήκευση email και username στη βάση δεδομένων
     data = {
         "gender": gender,
